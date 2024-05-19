6cab01aae101c7898a28ab3526d9785bca4d34fc
Mikki Michaelis <mikki@psychward.software>
2024-05-19 03:22:09 -0500

    firebase

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ExternalLinkDirective } from '../../directives/external-link.directive';
 import { ArrowBoxIconComponent } from '../../components/icons/arrow-box-icon.component';
 import { GithubIconComponent } from '../../components/icons/github-icon.component';
 import { TwitterIconComponent } from '../../components/icons/twitter-icon.component';
 import { YouTubeIconComponent } from '../../components/icons/youtube-icon.component';
+import zoomSdk from "@zoom/appssdk"
 
 const icons = [ArrowBoxIconComponent, GithubIconComponent, TwitterIconComponent, YouTubeIconComponent];
 
@@ -18,6 +19,13 @@ const icons = [ArrowBoxIconComponent, GithubIconComponent, TwitterIconComponent,
     'class': 'content'
   }
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  async ngOnInit() {
+    const configResponse = await zoomSdk.config({
+      popoutSize: {width: 480, height: 360},
+      capabilities: ["shareApp"]
+    });
 
+    console.log(configResponse);
+  }
 }
